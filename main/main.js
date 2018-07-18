067871c3f0492e42e8b17971df6787f550a069c0
Di Yuan <dyuan@thoughtworks.com>
2018-07-18 21:57:23 +0800

    [Yuan Di]task2 - getTotal list from input

--- main/main.js
+++ main/main.js
@@ -1,4 +1,4 @@
-module.exports = function printInventory() {
+module.exports = function printInventory(inputs) {
     var result =
         '***<没钱赚商店>购物清单***\n' +
         '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
@@ -12,6 +12,26 @@ module.exports = function printInventory() {
         '总计：51.00(元)\n' +
         '节省：7.50(元)\n' +
         '**********************';
-
     console.log(result);
 };
+
+function getTotal(inputs) {
+    const result = [];
+    const items = require('./datbase').loadAllItems();
+    inputs.forEach(
+        item => {
+            const barcode = item.split("-")[0];
+            const count = item.split("-").length > 1 ? parseInt(item.split("-")[1]) : 1;
+            const receiptItem = result.filter(itemInResult => itemInResult.item.barcode === barcode).pop();
+            if (receiptItem) {
+                receiptItem.count += count;
+            } else {
+                result.push({
+                    item: items.filter(itemInList => itemInList.barcode === barcode).pop(),
+                    count: count
+                });
+            }
+        }
+    );
+    return result;
+}
