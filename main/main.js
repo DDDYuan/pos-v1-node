5913569b401a92772cac3a36a0d76e5854e2c9bc
Di Yuan <dyuan@thoughtworks.com>
2018-07-18 22:22:22 +0800

    [Yuan Di]task4 -generate receipt

--- main/main.js
+++ main/main.js
@@ -1,18 +1,8 @@
 module.exports = function printInventory(inputs) {
-    var result =
-        '***<没钱赚商店>购物清单***\n' +
-        '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-        '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
-        '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
-        '----------------------\n' +
-        '挥泪赠送商品：\n' +
-        '名称：雪碧，数量：1瓶\n' +
-        '名称：方便面，数量：1袋\n' +
-        '----------------------\n' +
-        '总计：51.00(元)\n' +
-        '节省：7.50(元)\n' +
-        '**********************';
-    console.log(result);
+    const total = getTotal(inputs);
+    const promotion = getPromotions(total);
+    const receipt = generateReceipt(total, promotion);
+    console.log(receipt);
 };
 
 function getTotal(inputs) {
@@ -49,5 +39,34 @@ function getPromotions(total) {
             }
         }
     );
+    return result;
+}
+
+function generateReceipt(total, promotions) {
+    let amount = 0;
+    let promotionAmout = 0;
+    let result = '***<没钱赚商店>购物清单***\n';
+    total.forEach(
+        item => {
+            const subtotal = item.item.price * item.count;
+            amount += subtotal;
+            result += `名称：${item.item.name}，数量：${item.count}${item.item.unit}，单价：${item.item.price.toFixed(2)}(元)，小计：${subtotal.toFixed(2)}(元)`;
+            result += '\n';
+        }
+    );
+    result += '----------------------\n挥泪赠送商品：\n';
+    promotions.forEach(
+        item => {
+            const subtotal = item.item.price * item.count;;
+            promotionAmout += subtotal;
+            result += `名称：${item.item.name}，数量：${item.count}${item.item.unit}`;
+            result += '\n';
+        }
+    );
+    result += '----------------------\n' +
+        `总计：${amount.toFixed(2)}(元)` + '\n' +
+        `节省：${promotionAmout.toFixed(2)}(元)` + '\n' +
+        '**********************';
+
     return result;
 }
